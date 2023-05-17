50efd883210ff10b60316f9c8085f1c13611ec2f
Max <maxhannan@quenelle.app>
2023-05-17 11:47:33 -0500

    manifest change

--- app/routes/_auth/route.tsx
+++ app/routes/_auth/route.tsx
@@ -1,10 +1,28 @@
-import { Outlet } from "@remix-run/react";
+import { MetaFunction } from "@remix-run/node";
+import { Meta, Outlet } from "@remix-run/react";
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Quenelle",
+  viewport:
+    "width=device-width,initial-scale=1, maximum-scale=1.0,user-scalable=0 ",
+  "apple-mobile-web-app-capable": "yes",
+
+  display: "standalone",
+  "mobile-web-app-capable": "yes",
+
+  "apple-touch-fullscreen": "yes",
+});
 
 const AuthLayout = () => {
   return (
-    <div className=" container max-w-2xl mx-auto h-screen w-screen flex justify-center items-center ">
-      <Outlet />
-    </div>
+    <html lang="en">
+      <head>
+        <Meta />
+      </head>
+      <div className=" container max-w-2xl mx-auto h-screen w-screen flex justify-center items-center ">
+        <Outlet />
+      </div>
+    </html>
   );
 };
 
--- app/routes/app/route.tsx
+++ app/routes/app/route.tsx
@@ -1,4 +1,5 @@
 import {
+  Meta,
   Outlet,
   useLoaderData,
   useLocation,
@@ -7,12 +8,25 @@ import {
 import { useState, useEffect } from "react";
 import BottomNav from "~/components/navigation/BottomNav";
 import ErrorBoundaryLayout from "./ErrorBoundary";
-import { LoaderArgs } from "@remix-run/node";
+import { LoaderArgs, MetaFunction } from "@remix-run/node";
 import { getUser, requireUserId } from "~/utils/auth.server";
 
 export function ErrorBoundary() {
   return <ErrorBoundaryLayout />;
 }
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Quenelle",
+  viewport:
+    "width=device-width,initial-scale=1, maximum-scale=1.0,user-scalable=0 ",
+  "apple-mobile-web-app-capable": "yes",
+
+  display: "standalone",
+  "mobile-web-app-capable": "yes",
+
+  "apple-touch-fullscreen": "yes",
+});
+
 export async function loader({ request }: LoaderArgs) {
   await requireUserId(request);
   const user = await getUser(request);
@@ -30,12 +44,17 @@ const AppLayout = () => {
     setPage(location.pathname.split("/")[2]);
   }, [location]);
   return (
-    <div className=" px-3 ">
-      <div className="container  mx-auto">
-        <Outlet context={{ user }} />
+    <html lang="en">
+      <head>
+        <Meta />
+      </head>
+      <div className=" px-3 ">
+        <div className="container  mx-auto">
+          <Outlet context={{ user }} />
+        </div>
+        <BottomNav page={page} setPage={setPage} />
       </div>
-      <BottomNav page={page} setPage={setPage} />
-    </div>
+    </html>
   );
 };
 
