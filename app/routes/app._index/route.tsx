9f56f2d6ad16c7a0270b7200166114dacaa95b2d
Max <maxhannan@quenelle.app>
2023-05-28 23:47:42 -0500

    homepage

--- app/routes/app._index/route.tsx
+++ app/routes/app._index/route.tsx
@@ -25,14 +25,14 @@ function HomeRoute() {
     );
   if (!user) return null;
   return (
-    <div className=" container mx-auto max-w-3xl flex flex-col  gap-2 mt-12 items-center mb-28">
+    <div className=" container mx-auto max-w-3xl flex flex-col  gap-2 mt-3 items-center mb-28">
       <div className="text-4xl md:text-4xl  gap-2 items-start  w-full flex justify-between   py-3  dark:text-neutral-200  font-bold text-neutral-600 rounded-xl ">
         <div>
           <h1>Hi {user.firstName}!</h1>
           <h2 className="text-base text-zinc-600">Welcome to your kitchen.</h2>
         </div>
         <div
-          className={`bg-indigo-500  text-zinc-800 dark:text-zinc-300 trasition-all duration-300 inline-flex h-16 w-16 group-hover:bg-indigo-500  group-hover:text-zinc-200  child flex-shrink-0 items-center mr-4 justify-center  overflow-hidden   rounded-2xl   `}
+          className={`bg-indigo-500  text-zinc-800 dark:text-zinc-300 trasition-all duration-300 inline-flex h-16 w-16 group-hover:bg-indigo-500  group-hover:text-zinc-200  child flex-shrink-0 items-center  justify-center  overflow-hidden   rounded-2xl   `}
         >
           <span className=" text-4xl  ">
             {user.firstName[0].toLowerCase() + user.lastName[0].toLowerCase()}
@@ -56,15 +56,15 @@ function HomeRoute() {
           user={"ej"}
           name="PM Grill"
         />
-        <div className="w-full bg-indigo-500 flex flex-col gap-2  text-xl font-light text-zinc-200 rounded-2xl p-3">
-          <div className="w-7/12 bg-indigo-600 p-3 rounded-xl">
-            <span className="font-bold">79</span> Breakfast Covers
+        <div className="w-full  flex flex-col gap-2  text-xl font-light text-zinc-200 rounded-2xl">
+          <div className="w-7/12 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-600  p-3 rounded-xl">
+            <span className="font-bold">79</span> Breakfast
           </div>
-          <div className="w-5/12 bg-indigo-600 p-3 rounded-xl">
-            <span className="font-bold">52</span> Lunch Covers
+          <div className="w-5/12 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-600 p-3 rounded-xl">
+            <span className="font-bold">52</span> Lunch
           </div>
-          <div className="w-12/12 bg-indigo-600 p-3 rounded-xl">
-            <span className="font-bold">157</span> Dinner Covers
+          <div className="w-12/12 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-600 p-3 rounded-xl">
+            <span className="font-bold">157</span> Dinner
           </div>
         </div>
         <div className="text-lg text-indigo-500 font-semibold ">
