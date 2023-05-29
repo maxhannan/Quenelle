93faf95e784f4eec1b8c7aef937036042c2046d5
Max <maxhannan@quenelle.app>
2023-05-29 00:07:11 -0500

    fix

--- app/routes/app._index/route.tsx
+++ app/routes/app._index/route.tsx
@@ -27,7 +27,7 @@ function HomeRoute() {
   if (!user) return null;
   return (
     <div className=" container mx-auto max-w-3xl flex flex-col  gap-2 mt-3  items-center mb-28">
-      <div className="text-4xl md:text-4xl  gap-2 items-center  w-full flex justify-between mb-2   dark:text-neutral-200  font-bold text-neutral-600 rounded-xl ">
+      <div className="text-4xl md:text-4xl  gap-2 items-center  w-full flex justify-between    dark:text-neutral-200  font-bold text-neutral-600 rounded-xl ">
         <div>
           <h1>Hi {user.firstName}!</h1>
           <h2 className="text-base text-zinc-600">Welcome to your kitchen.</h2>
@@ -41,11 +41,7 @@ function HomeRoute() {
         </div>
       </div>
       <div className="w-full flex flex-col gap-2">
-        <div className="w-full  flex  gap-2  text-xl font-light justify-between text-zinc-200 rounded-2xl ">
-          <div className="  rounded-xl flex gap-2 items-center justify-between">
-            <span className="font-bold">{new Date().toDateString()}</span>
-            <CalendarDaysIcon className="w-5 h-5 inline-block" />
-          </div>
+        <div className="w-full  flex  gap-2  text-xl font-light  text-zinc-200 rounded-2xl ">
           <div className=" p-3 bg-indigo-500 rounded-xl flex gap-2 items-center justify-between">
             <span className="font-bold">79°F</span>
             <SunIcon className="w-7 h-7 inline-block" />
