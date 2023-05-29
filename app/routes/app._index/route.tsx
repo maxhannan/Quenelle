4d6b39b45b07343a52a25b67e5f28a93910a7e7a
Max <maxhannan@quenelle.app>
2023-05-29 00:06:08 -0500

    fix

--- app/routes/app._index/route.tsx
+++ app/routes/app._index/route.tsx
@@ -1,6 +1,7 @@
-import { SunIcon } from "@heroicons/react/24/outline";
+import { CalendarDaysIcon, SunIcon } from "@heroicons/react/24/outline";
 import type { LoaderArgs } from "@remix-run/node";
 import { useLoaderData, useNavigation } from "@remix-run/react";
+import { Calendar } from "lucide-react";
 import Spinner from "~/components/LoadingSpinner";
 import ListCard from "~/components/display/ListCard";
 
@@ -26,7 +27,7 @@ function HomeRoute() {
   if (!user) return null;
   return (
     <div className=" container mx-auto max-w-3xl flex flex-col  gap-2 mt-3  items-center mb-28">
-      <div className="text-4xl md:text-4xl  gap-2 items-center  w-full flex justify-between   dark:text-neutral-200  font-bold text-neutral-600 rounded-xl ">
+      <div className="text-4xl md:text-4xl  gap-2 items-center  w-full flex justify-between mb-2   dark:text-neutral-200  font-bold text-neutral-600 rounded-xl ">
         <div>
           <h1>Hi {user.firstName}!</h1>
           <h2 className="text-base text-zinc-600">Welcome to your kitchen.</h2>
@@ -40,6 +41,16 @@ function HomeRoute() {
         </div>
       </div>
       <div className="w-full flex flex-col gap-2">
+        <div className="w-full  flex  gap-2  text-xl font-light justify-between text-zinc-200 rounded-2xl ">
+          <div className="  rounded-xl flex gap-2 items-center justify-between">
+            <span className="font-bold">{new Date().toDateString()}</span>
+            <CalendarDaysIcon className="w-5 h-5 inline-block" />
+          </div>
+          <div className=" p-3 bg-indigo-500 rounded-xl flex gap-2 items-center justify-between">
+            <span className="font-bold">79°F</span>
+            <SunIcon className="w-7 h-7 inline-block" />
+          </div>
+        </div>
         <div className="text-lg text-indigo-500 font-semibold ">
           Your prep list for today.
         </div>
