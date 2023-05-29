d23fcc16d70d68ce90d81380aabc0021428676fb
Max <maxhannan@quenelle.app>
2023-05-28 23:01:49 -0500

    homepage

--- app/routes/app._index/route.tsx
+++ app/routes/app._index/route.tsx
@@ -24,8 +24,18 @@ function HomeRoute() {
   if (!user) return null;
   return (
     <div className=" container mx-auto max-w-3xl flex  gap-2 mt-12 items-center">
-      <div className="text-4xl md:text-4xl flex-col gap-2 items-start  w-full flex justify-between mb-3   dark:text-neutral-200  font-bold text-neutral-600 rounded-xl ">
-        <h1>Hi {user.firstName}!</h1>
+      <div className="text-4xl md:text-4xl  gap-2 items-start  w-full flex justify-between mb-3  p-3  dark:text-neutral-200  font-bold text-neutral-600 rounded-xl ">
+        <div>
+          <h1>Hi {user.firstName}!</h1>
+          <h2 className="text-base text-zinc-600">Welcome to your kitchen.</h2>
+        </div>
+        <div
+          className={`bg-indigo-500  text-zinc-800 dark:text-zinc-300 trasition-all duration-300 inline-flex h-16 w-16 group-hover:bg-indigo-500  group-hover:text-zinc-200  child flex-shrink-0 items-center mr-4 justify-center  overflow-hidden   rounded-2xl   `}
+        >
+          <span className=" text-4xl  ">
+            {user.firstName[0].toLowerCase() + user.lastName[0].toLowerCase()}
+          </span>
+        </div>
       </div>
     </div>
   );
